8e594d6ac4d32d5f6f99247bd914bc644ed087a7
KenanRico <kenan.hu@mail.utoronto.ca>
2020-01-31 08:26:02 -0500

    successfully render edges; but it's incorrect.

--- renderenvironment.c
+++ renderenvironment.c
@@ -3,12 +3,15 @@
 
 #include "SDL2headers.h"
 
+#include <stdlib.h>
+
 
 #define OK 0
 #define BAD_WINDOW 0x1
 #define BAD_RENDERER 0x2
 #define BAD_IMAGE 0x4
 
+#define _MIN(x,y) (x)<(y)?(x):(y)
 
 
 int window_width = 400;
@@ -64,6 +67,9 @@ void re_update(RenderEnvironment* re){
 	SDL_GetWindowSize(re->window, &window_width, &window_height);
 }
 
+float slope(Graph const * g, int v1, int v2){
+	return (float)(g->v_pos_y[v1]-g->v_pos_y[v2]) / (float)(g->v_pos_x[v1]-g->v_pos_x[v2]);
+}
 
 void re_render(RenderEnvironment* re, EventHandler const * eh, Graph const * g){
 
@@ -76,11 +82,14 @@ void re_render(RenderEnvironment* re, EventHandler const * eh, Graph const * g){
 		SDL_RenderCopyEx(re->renderer, re->elements.vertex, &re->elements.vertex_src, &re->elements.vertex_dest, 0.0f, NULL, SDL_FLIP_NONE);
 		for(int j=0; j<g->size; ++j){
 			/*edge render placeholder*/
-			re->elements.edge_dest.x = 2*j;
-			re->elements.edge_dest.y = 2*j;
-			re->elements.edge_dest.w = 100;
-			re->elements.edge_dest.h = 100;
-			SDL_RenderCopyEx(re->renderer, re->elements.edge, &re->elements.edge_src, &re->elements.edge_dest, 0.0f, NULL, SDL_FLIP_NONE);
+			if(g->graph[i][j]){
+				re->elements.edge_dest.x = _MIN(g->v_pos_x[i], g->v_pos_x[j]);
+				re->elements.edge_dest.y = _MIN(g->v_pos_y[i], g->v_pos_y[j]);
+				re->elements.edge_dest.w = abs(g->v_pos_x[i]-g->v_pos_x[j]);
+				re->elements.edge_dest.h = abs(g->v_pos_y[i]-g->v_pos_y[j]);
+				SDL_RendererFlip flip = (slope(g,i,j)<0) ? SDL_FLIP_VERTICAL : SDL_FLIP_NONE;
+				SDL_RenderCopyEx(re->renderer, re->elements.edge, &re->elements.edge_src, &re->elements.edge_dest, 0.0f, NULL, flip);
+			}
 		}
 	}
 	SDL_RenderPresent(re->renderer);
